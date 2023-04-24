9bbd128852ba05ad14afc21b9f0ff36b2a592776
Pablo Mansanet <pablomansanet@gmail.com>
2023-04-24 15:00:17 +0200

    Retrieve stdout and stderr writes as events

--- crates/ptrace_probe/src/main.rs
+++ crates/ptrace_probe/src/main.rs
@@ -1,8 +1,7 @@
 use clap::Parser;
 use composer::api::{Client, Event};
-use eyre::{bail, Result};
+use eyre::Result;
 use nix::{
-    errno::Errno,
     sys::{
         ptrace::{self, Options},
         wait::waitpid,
@@ -24,34 +23,41 @@ struct Args {
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args = Args::parse();
-
     let pid = Pid::from_raw(args.pid as i32);
+
+    let client = args
+        .address
+        .map(Client::new)
+        .unwrap_or(Client::try_default())?;
 
     ptrace::attach(pid)?;
     waitpid(Some(pid), None)?;
     loop {
-        // Stop on syscall invocation
-        ptrace::syscall(pid, None)?;
-        waitpid(Some(pid), None)?;
+        if let Some(event) = handle_syscall(pid)? {
+            println!("{event:?}");
+        }
+    }
+}
 
-        let registers = ptrace::getregs(pid)?;
-        let syscall: Sysno = (registers.orig_rax as i32).into();
-        print!(
-            "{syscall}({}, {}, {}, {}, {}, {})",
-            registers.rdi, registers.rsi, registers.rdx, registers.r10, registers.r8, registers.r9
-        );
+fn handle_syscall(pid: Pid) -> Result<Option<Event>, color_eyre::Report> {
+    ptrace::syscall(pid, None)?;
+    waitpid(Some(pid), None)?;
+    let registers = ptrace::getregs(pid)?;
+    let syscall: Sysno = (registers.orig_rax as i32).into();
+    let rdi = registers.rdi;
 
-        // Run the syscall
-        ptrace::syscall(pid, None)?;
-        waitpid(Some(pid), None)?;
-        match ptrace::getregs(pid) {
-            Ok(registers) => println!(" = {}", registers.rax),
-            Err(e) => {
-                println!(" = ?");
-                if e == Errno::ESRCH {
-                    bail!("Probe target exited.");
-                }
-            }
-        };
+    // At this point, the syscall is suspended, we need to call `syscall`
+    // again to actually execute it and retrieve the return value from `rax`.
+    ptrace::syscall(pid, None)?;
+    waitpid(Some(pid), None)?;
+
+    match (syscall, rdi) {
+        (Sysno::write, 1) => Ok(Some(Event::StdoutWrite {
+            length: ptrace::getregs(pid)?.rax as usize,
+        })),
+        (Sysno::write, 2) => Ok(Some(Event::StderrWrite {
+            length: ptrace::getregs(pid)?.rax as usize,
+        })),
+        _ => Ok(None),
     }
 }
