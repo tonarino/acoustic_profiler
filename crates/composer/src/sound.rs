4e0449a15a26ac88f7f361276d68234f7a8c89d1
Matěj Laitl <matej@laitl.cz>
2023-04-27 23:15:40 +0200

    Refactor AudioOutput for race-free sound time-positioning
    This way is slightly more elaborate, but should be as precise as it gets - it uses playback timestamp from the audio system itself.

--- crates/composer/src/sound.rs
+++ crates/composer/src/sound.rs
@@ -1,26 +1,30 @@
 use cpal::{
     traits::{DeviceTrait, HostTrait},
-    SampleFormat,
+    OutputCallbackInfo, OutputStreamTimestamp, SampleFormat, StreamInstant,
 };
 use eyre::{bail, eyre, Result};
-use rodio::{dynamic_mixer::DynamicMixerController, Source};
+use rodio::{
+    dynamic_mixer::{DynamicMixer, DynamicMixerController},
+    Source,
+};
 use std::{
     sync::{
         atomic::{AtomicU64, Ordering},
+        mpsc::{channel, Receiver, Sender, TryRecvError},
         Arc,
     },
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 pub(crate) struct AudioOutput {
-    mixer_controller: Arc<DynamicMixerController<f32>>,
-    /// UNIX timestamp in nanoseconds, as an atomic variable.
-    last_callback_timestamp_ns: Arc<AtomicU64>,
+    source_tx: Sender<TimedSource>,
+    play_delay: Duration,
+    too_early_plays: Arc<AtomicU64>,
     _stream: cpal::Stream,
 }
 
 impl AudioOutput {
-    pub(crate) fn new() -> Result<Self> {
+    pub(crate) fn new(play_delay: Duration) -> Result<Self> {
         let cpal_device = cpal::default_host()
             .default_output_device()
             .ok_or_else(|| eyre!("no cpal audio output device found"))?;
@@ -36,48 +40,137 @@ impl AudioOutput {
             bail!("Only F32 sample format supported for now.");
         }
 
-        let (mixer_controller, mut mixer) =
+        let (mixer_controller, mixer) =
             rodio::dynamic_mixer::mixer::<f32>(stream_config.channels, stream_config.sample_rate.0);
 
-        let last_callback_timestamp_ns = Arc::new(AtomicU64::new(0));
+        let (source_tx, source_rx) = channel();
 
-        let last_callback_timestamp_ns_alias = Arc::clone(&last_callback_timestamp_ns);
+        let too_early_plays = Arc::default();
+
+        // The mixer_controller can be shared between threads, but we want to precisely control
+        // when we add new sources w.r.t. the audio callback, so we move it to the audio thread and
+        // use a mpsc channel to send new sources to the audio thread.
+        let mut audio_callback =
+            AudioCallback::new(mixer_controller, mixer, source_rx, &too_early_plays);
         let _stream = cpal_device.build_output_stream::<f32, _, _>(
             &stream_config,
-            move |data_out, _info| {
-                // println!("data_callback: date_out.len(): {}, info: {_info:?}.", data_out.len());
-
-                // TODO(Matej): can we use timestamps in _info instead?
-                // TODO(Matej): specify less strict ordering (in all places for this atomic var)
-                last_callback_timestamp_ns_alias.store(current_timestamp_ns(), Ordering::SeqCst);
-
-                data_out.iter_mut().for_each(|d| *d = mixer.next().unwrap_or(0f32))
-            },
+            move |data_out, info| audio_callback.fill_data(data_out, info),
             |err| eprintln!("Got cpal stream error callback: {err}."),
             None,
         )?;
 
-        Ok(Self { mixer_controller, last_callback_timestamp_ns, _stream })
+        Ok(Self { source_tx, play_delay, too_early_plays, _stream })
     }
 
     pub(crate) fn play<S: Source<Item = f32> + Send + 'static>(&self, source: S) {
-        let ns_since_last_callback = current_timestamp_ns()
-            .saturating_sub(self.last_callback_timestamp_ns.load(Ordering::SeqCst));
-        // println!("ns_since_last_callback: {ns_since_last_callback:>9}.",);
+        // TODO(Matej): use timestamp from the event itself once we have it.
+        let play_at_timestamp = current_timestamp() + self.play_delay;
 
-        // We assume that cpal's audio callbacks come at a steady rate, so we delay sounds to play
-        // by the time since last audio callback, so that it gets played at correct offset during
-        // the *next* callback. This is needed to accurately position sounds (sub buffer level).
-        // https://github.com/tonarino/acoustic_profiler/issues/19#issuecomment-1522348735
-        self.mixer_controller.add(source.delay(Duration::from_nanos(ns_since_last_callback)));
+        // TODO(Matej): we are in fact double-boxing because DynamicMixerController internally adds
+        // another box. But we need a sized type to send it through threads. We could make this
+        // method non-generic, but that would be less flexible, so just accept it for now.
+        let source = Box::new(source);
+
+        self.source_tx
+            .send(TimedSource { source, play_at_timestamp })
+            .expect("source receiver should be still alive");
+    }
+
+    /// Get "too early plays" counter since the last call of this method.
+    pub(crate) fn fetch_too_early_plays(&self) -> u64 {
+        self.too_early_plays.swap(0, Ordering::SeqCst)
     }
 }
 
-fn current_timestamp_ns() -> u64 {
-    let duration_since_epoch =
-        SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current UNIX time");
-    duration_since_epoch
-        .as_nanos()
-        .try_into()
-        .expect("Cannot convert current UNIX timestamp in nanoseconds into u64")
+/// An f32 [rodio::source::Source] with UNIX timestamp of desired play time attached.
+struct TimedSource {
+    source: Box<dyn Source<Item = f32> + Send + 'static>,
+    play_at_timestamp: Duration,
+}
+
+/// A sort of manual implementation of the closure used as cpal audio data callback, for tidiness.
+struct AudioCallback {
+    mixer_controller: Arc<DynamicMixerController<f32>>,
+    mixer: DynamicMixer<f32>,
+    source_rx: Receiver<TimedSource>,
+    too_early_plays: Arc<AtomicU64>,
+    stream_start: Option<StreamStart>,
+}
+
+impl AudioCallback {
+    fn new(
+        mixer_controller: Arc<DynamicMixerController<f32>>,
+        mixer: DynamicMixer<f32>,
+        source_rx: Receiver<TimedSource>,
+        too_early_plays: &Arc<AtomicU64>,
+    ) -> Self {
+        let too_early_plays = Arc::clone(too_early_plays);
+        Self { mixer_controller, mixer, source_rx, too_early_plays, stream_start: None }
+    }
+
+    fn fill_data(&mut self, data_out: &mut [f32], info: &OutputCallbackInfo) {
+        let stream_timestamp = info.timestamp();
+        let stream_start =
+            self.stream_start.get_or_insert_with(|| StreamStart::new(stream_timestamp));
+
+        // At least on Linux ALSA, cpal gives very strange stream timestamp on very first call.
+        if stream_start.callback > stream_timestamp.callback {
+            eprintln!("cpal's stream timestamp jumped backwards, resetting stream start.");
+            *stream_start = StreamStart::new(stream_timestamp);
+        }
+
+        // UNIX timestamp of when the buffer filled during this call will be actually played.
+        let buffer_playback_timestamp = stream_start.realtime
+            + (stream_timestamp.playback.duration_since(&stream_start.callback).expect(
+                "current playback timestamp should be larger than start callback timestamp",
+            ));
+
+        // Add possible new sources to the list
+        loop {
+            match self.source_rx.try_recv() {
+                Ok(timed_source) => {
+                    let delay = timed_source
+                        .play_at_timestamp
+                        .checked_sub(buffer_playback_timestamp)
+                        .unwrap_or_else(|| {
+                            self.too_early_plays.fetch_add(1, Ordering::SeqCst);
+                            Duration::ZERO
+                        });
+                    self.mixer_controller.add(timed_source.source.delay(delay));
+                },
+                Err(TryRecvError::Empty) => break,
+                Err(TryRecvError::Disconnected) => panic!("source sender should be still alive"),
+            }
+        }
+
+        data_out.iter_mut().for_each(|d| *d = self.mixer.next().unwrap_or(0f32))
+    }
+}
+
+/// An utility struct that anchors stream's internal timestamp to real world UNIX timestamp.
+struct StreamStart {
+    /// Timestamp of the start of stream _playback_ as the `Duration` since the UNIX epoch.
+    realtime: Duration,
+    /// Internal cpal's callback timestamp of stream start.
+    callback: StreamInstant,
+}
+
+impl StreamStart {
+    fn new(stream_timestamp: OutputStreamTimestamp) -> Self {
+        let realtime = current_timestamp();
+        println!(
+            "Audio stream starting at {realtime:?} UNIX timestamp, callback timestamp {:?}, \
+            playback timestamp delayed by {:?} after callback.",
+            stream_timestamp.callback,
+            stream_timestamp.playback.duration_since(&stream_timestamp.callback)
+        );
+
+        // We record _callback_ timestamp for the purposes of fixing playback in real time.
+        Self { realtime, callback: stream_timestamp.callback }
+    }
+}
+
+// Get current timestamp as the `Duration` since the UNIX epoch.
+fn current_timestamp() -> Duration {
+    SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current UNIX time")
 }
